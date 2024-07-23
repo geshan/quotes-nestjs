7929c52aba1e19a3b312c01371cbcd62b29ccada
Geshan Manandhar <geshan@simplywallst.com>
2024-07-23 21:45:58 +1000

    formats code

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -22,11 +22,11 @@ describe('AppController', () => {
 
   describe('quotes', () => {
     it('should return quotes with author john by default"', () => {
-      const response:any = appController.getQuotes('john');
+      const response: any = appController.getQuotes('john');
       expect(response).toHaveProperty('data');
       expect(response.data).toHaveLength(2);
-      expect(response.data[0]).toEqual({"author": "John Johnson", "quote": "First, solve the problem. Then, write the code."});
-      expect(response.data[1]).toEqual({"author": "John Woods", "quote": "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live."});
+      expect(response.data[0]).toEqual({ "author": "John Johnson", "quote": "First, solve the problem. Then, write the code." });
+      expect(response.data[1]).toEqual({ "author": "John Woods", "quote": "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live." });
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
@@ -18,28 +18,28 @@ export class AppController {
       {
         quote: 'There are only two kinds of languages: the ones people complain about and the ones nobody uses.',
         author: 'Bjarne Stroustrup'
-    },
-    {
+      },
+      {
         quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
         author: 'Martin Fowler'
-    },
-    {
+      },
+      {
         quote: 'First, solve the problem. Then, write the code.',
         author: 'John Johnson'
-    },
-    {
+      },
+      {
         quote: 'Java is to JavaScript what car is to Carpet.',
         author: 'Chris Heilmann'
-    },
-    {
+      },
+      {
         quote: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.',
         author: 'John Woods'
-    },
+      },
     ];
 
-    const filteredQuotes = author ? 
+    const filteredQuotes = author ?
       quotes.filter(quote => quote.author.toLocaleLowerCase().includes(author.toLocaleLowerCase())) :
       quotes;
-    return {"data": filteredQuotes};
+    return { "data": filteredQuotes };
   }
 }
