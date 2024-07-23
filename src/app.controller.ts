4f49cc9b9316c3138b53292b90ca81ac273e05c6
Geshan Manandhar <geshan@simplywallst.com>
2024-07-23 21:35:15 +1000

    Add a quotes route with query param and default value

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, DefaultValuePipe, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,34 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/quotes')
+  getQuotes(
+    @Query('author', new DefaultValuePipe('martin')) author: string,
+  ): Object {
+    const quotes = [
+      {
+        quote: 'There are only two kinds of languages: the ones people complain about and the ones nobody uses.',
+        author: 'Bjarne Stroustrup'
+    },
+    {
+        quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        author: 'Martin Fowler'
+    },
+    {
+        quote: 'First, solve the problem. Then, write the code.',
+        author: 'John Johnson'
+    },
+    {
+        quote: 'Java is to JavaScript what car is to Carpet.',
+        author: 'Chris Heilmann'
+    },
+    {
+        quote: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.',
+        author: 'John Woods'
+    },
+    ];
+    return {"data": quotes.filter(quote => quote.author.toLocaleLowerCase().includes(author.toLocaleLowerCase()))};
   }
 }
